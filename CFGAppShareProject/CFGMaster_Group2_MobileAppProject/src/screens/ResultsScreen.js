e8cbbf3e9fe5f6278639814138d3aefeaa8a1c38
WittK95 <108486235+WittK95@users.noreply.github.com>
2023-06-29 20:11:16 +0100

    added grade - merit, pass etc

--- CFGAppShareProject/CFGMaster_Group2_MobileAppProject/src/screens/ResultsScreen.js
+++ CFGAppShareProject/CFGMaster_Group2_MobileAppProject/src/screens/ResultsScreen.js
@@ -1,10 +1,13 @@
 import React from "react";
-import { styleSheet, Button, TextInput, View, Text } from "react-native";
+import { Button, TextInput, View, Text } from "react-native";
 import { Formik } from "formik";
 import { useState } from "react";
+import styles from "../styles/AppStyles.js";
+import { Keyboard } from "react-native";
 
 export default function Form() {
   const [finalGrade, setFinalGrade] = useState("");
+  const [gradeBand, setGradeBand] = useState("");
 
   return (
     <View>
@@ -25,51 +28,75 @@ export default function Form() {
               Number(values.value5) * 2.5) /
               10
           );
+
           setFinalGrade(result);
+
+          if (result < 49) {
+            setGradeBand("Fail 😔");
+          } else if (result > 50 && result < 69) {
+            setGradeBand("Pass 😊");
+          } else if (result > 70 && result < 89) {
+            setGradeBand("Merit 😄");
+          } else if (result > 90) {
+            setGradeBand("Distinction 😎");
+          }
+
+          Keyboard.dismiss();
         }}
       >
         {(props) => (
-          <View>
-            <Text>First assessment grade (10%):</Text>
+          <View style={styles.card}>
+            <Text style={styles.cardHeader}>Code First Girls</Text>
+            <Text style={styles.cardBody}>Grade calculator</Text>
+            <Text style={styles.cardBody}>First assessment grade (10%):</Text>
             <TextInput
+              style={styles.input}
               placeholder="value1"
               onChangeText={props.handleChange("value1")}
               value={props.values.value1}
               keyboardType="numeric"
             />
-            <Text>Second assessment grade (25%):</Text>
+            <Text style={styles.cardBody}>Second assessment grade (25%):</Text>
             <TextInput
+              style={styles.input}
               placeholder="value2"
               onChangeText={props.handleChange("value2")}
               value={props.values.value2}
               keyboardType="numeric"
             />
-            <Text>Homework average (15%):</Text>
+            <Text style={styles.cardBody}>Homework average (15%):</Text>
             <TextInput
+              style={styles.input}
               placeholder="value3"
               onChangeText={props.handleChange("value3")}
               value={props.values.value3}
               keyboardType="numeric"
             />
-            <Text>Group project result (25%):</Text>
+            <Text style={styles.cardBody}>Group project result (25%):</Text>
             <TextInput
+              style={styles.input}
               placeholder="value4"
               onChangeText={props.handleChange("value4")}
               value={props.values.value4}
               keyboardType="numeric"
             />
-            <Text>Specialisation exam result (25%):</Text>
+            <Text style={styles.cardBody}>
+              Specialisation exam result (25%):
+            </Text>
             <TextInput
+              style={styles.input}
               placeholder="value5"
               onChangeText={props.handleChange("value5")}
               value={props.values.value5}
               keyboardType="numeric"
             />
             <Button
+              style={styles.button}
               title="calculate final grade"
               onPress={props.handleSubmit}
             ></Button>
-            <Text>Final Grade: {finalGrade}</Text>
+            <Text style={styles.cardBody}>Final Grade: {finalGrade}%</Text>
+            <Text style={styles.cardBody}>{gradeBand}</Text>
           </View>
         )}
       </Formik>
--- CFGAppShareProject/CFGMaster_Group2_MobileAppProject/src/styles/AppStyles.js
+++ CFGAppShareProject/CFGMaster_Group2_MobileAppProject/src/styles/AppStyles.js
@@ -1,54 +1,54 @@
 // AppStyles.js
-import { StyleSheet } from 'react-native';
+import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
-    body: {
-        fontFamily: "Barlow",
-        backgroundColor: '#000',
-        color: '#333',
+  body: {
+    fontFamily: "Barlow",
+    backgroundColor: "#000",
+    color: "#333",
+  },
+  button: {
+    padding: 10,
+    fontSize: 16,
+    textAlign: "center",
+    borderRadius: 4,
+    backgroundColor: "#000",
+    color: "#fff",
+  },
+  input: {
+    width: "100%",
+    padding: 10,
+    fontSize: 16,
+    borderColor: "#ccc",
+    borderWidth: 1,
+    borderRadius: 4,
+  },
+  card: {
+    flexDirection: "column",
+    width: "100%",
+    padding: 20,
+    borderRadius: 8,
+    backgroundColor: "#ff69b4",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
     },
-    button: {
-        padding: 10,
-        fontSize: 16,
-        textAlign: 'center',
-        borderRadius: 4,
-        backgroundColor: '#000',
-        color: '#fff',
-    },
-    input: {
-        width: '100%',
-        padding: 10,
-        fontSize: 16,
-        borderColor: '#ccc',
-        borderWidth: 1,
-        borderRadius: 4,
-    },
-    card: {
-        flexDirection: 'column',
-        width: '100%',
-        padding: 20,
-        borderRadius: 8,
-        backgroundColor: '#ff69b4',
-        shadowColor: "#000",
-        shadowOffset: {
-            width: 0,
-            height: 2,
-        },
-        shadowOpacity: 0.1,
-        shadowRadius: 4,
-        elevation: 5,
-    },
-    cardHeader: {
-        fontSize: 100,
-        color: '#fff',
-        fontWeight: 'bold',
-        marginBottom: 10,
-    },
-    cardBody: {
-        flexGrow: 1,
-        color: '#000',
-    },
-    // ... and so on for the rest of your styles
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  cardHeader: {
+    fontSize: 40,
+    color: "#fff",
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  cardBody: {
+    flexGrow: 1,
+    color: "#000",
+  },
+  // ... and so on for the rest of your styles
 });
 
 export default styles;
